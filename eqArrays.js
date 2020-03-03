808d269b453dd50aeb3d7360245b7014b9ca244c
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-03 20:10:39 +0000

    eqArrays.js compare arrays and assess

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    return `🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`;
+  } else {
+    return `✅✅✅ Assertion passed: ${actual} === ${expected}`;
+
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+};
+
+// TEST CODE
+console.log(assertEqual(eqArrays([1, 3], [4, 2]), false));
+console.log(assertEqual(eqArrays([1, 3, 4], [1, 3, 4, 5]), false));
