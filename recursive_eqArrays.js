4f7e523f0ff89659e788b93ff1e655524efe73c7
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-07 00:46:15 +0000

    eqArrays with recursive element

---
+++ recursive_eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+ 
+  for (const i of arr1) {
+    if (Array.isArray(arr1[i])) {
+      eqArrays(arr1[i], arr2[i]);
+    }
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertEqual(eqArrays([[2],[[4, 3]], 3], [[2],[[4, 3]], 3]), true);
