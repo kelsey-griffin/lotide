c24340d41a28f675eaa34b01d22baa7af8f4a49b
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-09 17:22:07 +0000

    added module for eqArrays and eqArrays test for testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    return `🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`;
-  } else {
-    return `✅✅✅ Assertion passed: ${actual} === ${expected}`;
-
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length){ 
@@ -20,6 +12,4 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-// TEST CODE
-console.log(assertEqual(eqArrays([1, 3], [4, 2]), false));
-console.log(assertEqual(eqArrays([1, 3, "4"], [1, 3, 4]), false));
+module.exports = eqArrays;
