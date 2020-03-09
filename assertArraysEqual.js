2db526b1bb4c425eb482e98cf4cf2cedcc0648d5
Kelsey Griffin <kjulegriffin@gmail.com>
2020-03-09 17:34:11 +0000

    added module to assertArraysEqual and created file assertArraysEqualTest to test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays');
+
 // FUNCTION IMPLEMENTATION
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
@@ -7,17 +9,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-};
-
-// TEST CODE
-assertArraysEqual([1, 3], [4, 2]);
-assertArraysEqual([1, 3, 4], [1, 3, 4]);
+module.exports = assertArraysEqual;
